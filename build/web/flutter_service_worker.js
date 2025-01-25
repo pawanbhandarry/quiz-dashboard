e0be37aaa2a90c87c1c600ed6d05cabc650036de
codxeeorg <codxee@gmail.com>
2025-01-25 18:42:15 +0545

    build issue

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -90,7 +90,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "2d671900e9e3e2e9fa5f50602f684b40
 "canvaskit/skwasm.worker.js": "89990e8c92bcb123999aa81f7e203b1c",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "4b2350e14c6650ba82871f60906437ea",
-"flutter_bootstrap.js": "50a1438337358ee8bfa67912b5220384",
+"flutter_bootstrap.js": "0aa64453646b7395785a4c7192382b21",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
