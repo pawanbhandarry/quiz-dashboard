826c22e36c9a078978f74169f307dbd97cb46a86
codxeeorg <codxee@gmail.com>
2025-01-27 20:58:07 +0545

    deployement changes

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cb4b5fff73850b2e42bd4de7cb9a4310a78ac4
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3539901347"
+    serviceWorkerVersion: "1137585875"
   }
 });
